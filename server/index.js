4acf294f00d3c60f14445d8e1b1a72636d12c5e3
actuharry <actuharry@gmail.com>
2020-02-18 00:06:48 -0800

    testing database stuff

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const Knex = require('knex');
 
-const directions = require('./directions.js');
 const results = require('./results.js');
+
+// const directions = require('./directions.js');
 
 // var inputs = {
 //   origin: '1600 Amphitheatre Parkway, Mountain View, CA',
@@ -26,6 +29,75 @@ if (env === 'development') {
   app.use(cors(corsOptions));
 }
 
+// [START cloud_sql_postgres_knex_create]
+// Initialize Knex, a Node.js SQL query builder library with built-in connection pooling.
+const connect = () => {
+  // Configure which instance and what database user to connect with.
+  // Remember - storing secrets in plaintext is potentially unsafe. Consider using
+  // something like https://cloud.google.com/kms/ to help keep secrets secret.
+  const config = {
+    user: process.env.DB_USER, // e.g. 'my-user'
+    password: process.env.DB_PASS, // e.g. 'my-user-password'
+    database: process.env.DB_NAME // e.g. 'my-database'
+  };
+
+  config.host = `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`;
+
+  // Establish a connection to the database
+  const knex = Knex({
+    client: 'pg',
+    connection: config
+  });
+
+  // ... Specify additional properties here.
+  // [START_EXCLUDE]
+
+  // [START cloud_sql_postgres_knex_limit]
+  // 'max' limits the total number of concurrent connections this pool will keep. Ideal
+  // values for this setting are highly variable on app design, infrastructure, and database.
+  knex.client.pool.max = 5;
+  // 'min' is the minimum number of idle connections Knex maintains in the pool.
+  // Additional connections will be established to meet this value unless the pool is full.
+  knex.client.pool.min = 5;
+  // [END cloud_sql_postgres_knex_limit]
+  // [START cloud_sql_postgres_knex_timeout]
+  // 'acquireTimeoutMillis' is the maximum number of milliseconds to wait for a connection checkout.
+  // Any attempt to retrieve a connection from this pool that exceeds the set limit will throw an
+  // SQLException.
+  knex.client.pool.createTimeoutMillis = 30000; // 30 seconds
+  // 'idleTimeoutMillis' is the maximum amount of time a connection can sit in the pool. Connections that
+  // sit idle for this many milliseconds are retried if idleTimeoutMillis is exceeded.
+  knex.client.pool.idleTimeoutMillis = 600000; // 10 minutes
+  // [END cloud_sql_postgres_knex_timeout]
+  // [START cloud_sql_postgres_knex_backoff]
+  // 'createRetryIntervalMillis' is how long to idle after failed connection creation before trying again
+  knex.client.pool.createRetryIntervalMillis = 200; // 0.2 seconds
+  // [END cloud_sql_postgres_knex_backoff]
+  // [START cloud_sql_postgres_knex_lifetime]
+  // 'acquireTimeoutMillis' is the maximum possible lifetime of a connection in the pool. Connections that
+  // live longer than this many milliseconds will be closed and reestablished between uses. This
+  // value should be several minutes shorter than the database's timeout value to avoid unexpected
+  // terminations.
+  knex.client.pool.acquireTimeoutMillis = 600000; // 10 minutes
+  // [START cloud_sql_postgres_knex_lifetime]
+
+  // [END_EXCLUDE]
+  return knex;
+};
+
+const knex = connect();
+// [END cloud_sql_postgres_knex_create]
+
+/**
+ * Retrieve the latest 5 vote records from the database.
+ *
+ * @param {object} knex The Knex connection object.
+ * @returns {Promise}
+ */
+const getData = async knex => {
+  return await knex.select('*').from('guestbook');
+};
+
 app.get('/', async (req, res) => {
   try {
     let startingPoint = decodeURIComponent(req.query.startingPoint);
@@ -37,6 +109,8 @@ app.get('/', async (req, res) => {
     //   mode: 'driving',
     //   departure_time: 'now'
     // };
+    const databaseData = await getData(knex);
+    console.log(databaseData);
 
     // let result = await directions.getDirections(inputs);
     let response = await results.getResults(req);
