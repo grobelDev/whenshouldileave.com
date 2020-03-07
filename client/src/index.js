ae6ab7449c6229ebf3fa0b8e6bd06b8efe5bcf44
grobelDev <56764278+grobelDev@users.noreply.github.com>
2020-03-06 19:42:49 -0800

    added helmet

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import './tailwind.css';
 
@@ -8,6 +9,12 @@ import App from './App';
 
 ReactDOM.render(
   <Router>
+    <Helmet>
+      <meta charSet='utf-8' />
+      <title>When Should I Leave</title>
+      <link rel='canonical' href='http://whenshouldileave.com' />
+    </Helmet>
+
     <App />
   </Router>,
   document.getElementById('root')
